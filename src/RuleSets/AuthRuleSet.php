9472882cd6fa0643f88e742fa51daeb49750da67
Reza Moradi <rezamoradix@gmail.com>
2023-09-21 11:55:51 +0330

    Update AuthRuleSet.php

--- src/RuleSets/AuthRuleSet.php
+++ src/RuleSets/AuthRuleSet.php
@@ -4,32 +4,62 @@ namespace Rey\Validation\RuleSets;
 
 class AuthRuleSet
 {
+    // Private
+    private const _PASSWORD = [
+        'label' => 'Validation.password',
+        'rules' => 'required|min_length[6]|max_length[180]'
+    ];
+
+    private const _EMAIL = [
+        'label' => 'Validation.email',
+        'rules' => 'required|valid_email|max_length[180]'
+    ];
+
+    private const _PHONE = [
+        'label' => 'Validation.phone',
+        'rules' => 'required|iranphone'
+    ];
+
+
+    // Public
     public const USER_PASS = [
         'username' => [
             'label' => 'Validation.username',
-            'rules' => 'required'
+            'rules' => 'required|min_length[3]|max_length[180]'
         ],
-        'password' => [
-            'label' => 'Validation.password',
-            'rules' => 'required'
-        ]
+        'password' => self::_PASSWORD
     ];
 
     public const EMAIL_PASS = [
-        'email' => [
-            'label' => 'Validation.email',
-            'rules' => 'required|valid_email'
-        ],
-        'password' => [
-            'label' => 'Validation.password',
-            'rules' => 'required'
-        ]
+        'email' => self::_EMAIL,
+        'password' => self::_PASSWORD
     ];
 
     public const PHONE = [
-        'phone' => [
-            'label' => 'Validation.phone',
-            'rules' => 'required|iranphone'
+        'phone' => self::_PHONE
+    ];
+
+    public const PHONE_CODE_5 = [
+        'phone_code' => [
+            'label' => 'Validation.phone_code',
+            'rules' => 'required|numeric|exact_length[5]'
         ],
+    ];
+
+    public const PHONE_CODE_6 = [
+        'phone_code' => [
+            'label' => 'Validation.phone_code',
+            'rules' => 'required|numeric|exact_length[6]'
+        ],
+    ];
+
+    public const NAME_PHONE_EMAIL_PASS = [
+        'name' => [
+            'label' => 'Validation.name',
+            'rules' => 'required|min_length[3]|max_length[180]'
+        ],
+        'phone' => self::_PHONE,
+        'email' => self::_EMAIL,
+        'password' => self::_PASSWORD
     ];
 }
